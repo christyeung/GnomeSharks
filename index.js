fe4236ab75de9894bc07a45f81c5a3432e7727dd
Christy Yeung <yplc0603@gmail.com>
2021-08-22 11:55:36 -0700

    update

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const myGnome = document.getElementById('my-gnome');
 const myButton = document.getElementById('my-button');
 
 function getRandomGnome() {
-    const pics = ['gnome_01.jgp', 'gnome_02.jgp', 'gnome_03.jgp'];
-    const randomIndex = Math.floor(Math.random() * pics.length);
-    return '<img src="images/' + pics[randomIndex] + '"/>';
+    const pics = ['gnome_1.jpg', 'gnome_2.jpg', 'gnome_3.jpg'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return '<img src="images/' + pics[randomIndex] + '" class="gnome-photo"/>';
 }
 
 myButton.addEventListener('click', () => {
