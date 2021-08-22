77083a92cf87e47156b2943ccd3ca7432d339d2c
Christy Yeung <yplc0603@gmail.com>
2021-08-22 12:43:26 -0700

    update

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const myGnome = document.getElementById('my-gnome');
 const myButton = document.getElementById('my-button');
 
 function getRandomGnome() {
-    const pics = ['gnome_1.jpg', 'gnome_2.jpg', 'gnome_3.jpg', 'gnome_4.jpeg', 'gnome_5.jpeg', 'gnome_6.jpg', 'gnome_7.jpg', 'gnome_8.jpg'];
+    const pics = ['gnome_1.webp', 'gnome_2.jpg', 'gnome_3.jpg', 'gnome_4.jpeg', 'gnome_5.jpeg', 'gnome_6.jpg', 'gnome_7.jpg', 'gnome_8.jpg'];
     const randomIndex = Math.floor(Math.random() * pics.length);
+    myButton.disabled = true;
     return '<img src="images/' + pics[randomIndex] + '" class="gnome-photo"/>';
 }
 
